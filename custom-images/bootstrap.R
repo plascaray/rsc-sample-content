5a73e201203d7df4fa9a569405dfdc81f68ad5b8
David Kegley <david.kegley@rstudio.com>
2022-08-31 15:20:51 -0400

    add custom-images for testing and dockerfile

---
+++ custom-images/bootstrap.R
@@ -0,0 +1,7 @@
+# use binary packages
+local({r <- getOption("repos")
+       r["CRAN"] <- "https://packagemanager.rstudio.com/cran/__linux__/bionic/2022-08-31" 
+       options(repos=r)
+})
+
+install.packages(c("renv", "rmarkdown", "shiny", "dplyr", "plumber"))
