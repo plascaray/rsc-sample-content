9c3dd5db5dabfe52bc30c7b2ffe779a195719617
David Kegley <david.kegley@posit.co>
2024-05-22 17:24:42 -0400

    Add local streamlit app

---
+++ local/python-streamlit/app.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# mypy: ignore-errors
+import os
+
+import streamlit as st
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+
+st.write("Headers:")
+st.write(_get_websocket_headers())
+st.write("")
+st.write("Environment:")
+st.write(dict(os.environ))
+
